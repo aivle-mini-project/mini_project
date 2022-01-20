e614baeb912d76561ee04e0adb51741c3de4ab64
younglaecho <tks7205@gmail.com>
2022-01-21 00:47:54 +0900

    make base.html

--- board/apps.py
+++ board/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class StasticsConfig(AppConfig):
+class BoardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'stastics'
+    name = 'board'
--- config/settings.py
+++ config/settings.py
@@ -52,7 +52,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'EDuser',
     'diary',
-    'stastics',
     'bootstrap4'
 ]
 
