6445b99a496d7936c9726165cf313c7b8d4d8a0d
jin9007 <jjh970426@naver.com>
2022-01-21 16:29:33 +0900

    ~~~update

---
+++ stastics/models.py
@@ -0,0 +1,19 @@
+from tabnanny import verbose
+from django.db import models
+
+# Create your models here.
+class Stastics:
+  register_date = models.DateField()
+  good = models.IntegerField()
+  bad = models.IntegerField()
+  emotion1 = models.IntegerField()
+  emotion2 = models.IntegerField()
+  emotion3 = models.IntegerField()
+  emotion4 = models.IntegerField()
+
+  class Meta:
+    db_table='stastics'
+    verbose_name = '통계'
+    verbose_name_plural = '통계'
+
+
