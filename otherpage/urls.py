739a5fbc704342cdbe472a6916ee2536a32a1ea9
younglaecho <tks7205@gmail.com>
2022-01-25 16:59:07 +0900

    board expression ajax and django rest framework

--- otherpage/urls.py
+++ otherpage/urls.py
@@ -5,6 +5,5 @@ app_name = 'otherpage'
 
 urlpatterns = [
     path('show_date/', views.show_date, name='date'),
-    path('show_date/keyword', views.show_date_keyword, name='date_keyword'),
     path('show_week/', views.show_week, name='week'),
 ]
