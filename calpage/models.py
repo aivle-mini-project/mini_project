4049d9ca2bb93f081ec9b0f8797b040e4f59ee71
prowh08 <pro_wh08@naver.com>
2022-01-27 15:41:43 +0900

    22.01.27 login calendar

--- calpage/models.py
+++ calpage/models.py
@@ -1,12 +1,1 @@
-from django.db import models
-from django.db.models.fields import IntegerField
 
-class Calemotion(models.Model):
-    id = IntegerField(primary_key=True)
-    emo_date = models.DateField(null=False)
-    emotion = models.CharField(max_length=30,null=False)
-    emoji = models.CharField(max_length=30)
-
-    class Meta:
-        db_table = 'calemotion'
-        managed = False
